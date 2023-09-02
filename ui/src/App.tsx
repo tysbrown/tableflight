022edbe15b92c64fd1460548e0fd81acee67e648
Tyler Brown <tylershaynebrown@gmail.com>
2023-09-01 22:54:49 -0400

    chore(ui): clean up files

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-
+const App = () => {
   return (
     <>
-      <div>Tessssssaaaat</div>
+      <div>Hello, world!</div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
