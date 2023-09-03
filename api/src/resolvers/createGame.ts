cf45639fdea980c98617bab96042cadd55aa88ea
Tyler Brown <tylershaynebrown@gmail.com>
2023-09-03 02:33:01 -0400

    fix(api): add user relation to creategame resolver

--- api/src/resolvers/createGame.ts
+++ api/src/resolvers/createGame.ts
@@ -1,15 +1,21 @@
-import type { Game } from "@/common/types"
+// import type { Game } from "@/common/types"
+import type { Game } from "@prisma/client"
 import type { Context } from "../context"
 
 export const createGame = async (
   _: unknown,
   { name, description }: Partial<Game>,
-  context: Context
+  { prisma, user }: Context
 ) => {
-  const game = await context.prisma.game.create({
+  const game = await prisma.game.create({
     data: {
       name,
       description,
+      createdBy: {
+        connect: {
+          id: user.id,
+        },
+      },
     },
   })
   return game
