cb7c4a81180840cdf39a14b024abf668917a9db7
Tyler <tylershaynebrown@gmail.com>
2023-09-05 01:34:07 -0400

    build(ui): auth exchange dep

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './global.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./global.css"
+import { Client, Provider, cacheExchange, fetchExchange } from "urql"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const client = new Client({
+  url: import.meta.env.VITE_API_URL,
+  exchanges: [cacheExchange, fetchExchange],
+})
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
