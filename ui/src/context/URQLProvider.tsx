db8a552a91d2bcd52e2d1880ca4e1603a8e68782
Tyler Brown <tylershaynebrown@gmail.com>
2023-09-07 00:49:11 -0400

    feat(ui): urql provider

--- ui/src/context/URQLProvider.tsx
+++ ui/src/context/URQLProvider.tsx
@@ -1,54 +1,75 @@
 import type { Operation } from "urql"
 import type { AuthConfig, AuthUtilities } from "@urql/exchange-auth"
+import { useState } from "react"
 import { useGlobalStateContext } from "./useGlobalContext"
-import { Client, Provider, cacheExchange, fetchExchange } from "urql"
+import { Provider, createClient, fetchExchange } from "urql"
 import { authExchange } from "@urql/exchange-auth"
-import { useMemo } from "react"
+import { cacheExchange } from "@urql/exchange-graphcache"
+import { ClientContext } from "./ClientContext"
+import { useClient } from "../hooks/useClient"
 
 export default function URQLProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const { resetClient } = useClient()
   const { state, setState } = useGlobalStateContext()
-  const { isLoggedIn, accessToken } = state || {}
+  const { accessToken } = state || {}
 
-  const client = new Client({
-    url: import.meta.env.VITE_API_URL,
-    exchanges: [
-      cacheExchange,
-      authExchange(async (utils: AuthUtilities): Promise<AuthConfig> => {
-        return {
-          addAuthToOperation(operation: Operation) {
-            if (!accessToken) return operation
-            return utils.appendHeaders(operation, {
-              Authorization: `Bearer ${accessToken}`,
-            })
-          },
-          didAuthError: (error) => {
-            return error.graphQLErrors.some(
-              (e) => e.extensions?.code === "FORBIDDEN",
-            )
-          },
-          refreshAuth: async () => {
-            const response = await fetch(
-              import.meta.env.VITE_API_URL + "/refresh_token",
-              {
-                method: "POST",
-                credentials: "include",
-              },
-            )
-            const { accessToken, user } = await response.json()
-            setState({ ...state, accessToken, user, isLoggedIn: true })
-          },
-        }
-      }),
-      fetchExchange,
-    ],
-  })
+  const makeClient = () =>
+    createClient({
+      url: import.meta.env.VITE_API_URL,
+      exchanges: [
+        cacheExchange({}),
+        authExchange(async (utils: AuthUtilities): Promise<AuthConfig> => {
+          return {
+            addAuthToOperation(operation: Operation) {
+              if (!accessToken) return operation
+              return utils.appendHeaders(operation, {
+                Authorization: `Bearer ${accessToken}`,
+              })
+            },
+            didAuthError: (error) => {
+              return error.graphQLErrors.some(
+                (e) => e.extensions?.code === "FORBIDDEN",
+              )
+            },
+            refreshAuth: async () => {
+              const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/refresh_token`,
+                {
+                  method: "POST",
+                  credentials: "include",
+                },
+              )
+              const { accessToken, user } = await response.json()
 
-  // If user is not logged in, clear the urql cache/reset it manually
+              // If the refresh token is invalid, log the user out
+              if (!accessToken) {
+                setState({ ...state, isLoggedIn: false })
 
+                resetClient()
+                return
+              }
 
-  return <Provider value={client}>{children}</Provider>
+              setState({ ...state, accessToken, user, isLoggedIn: true })
+            },
+          }
+        }),
+        fetchExchange,
+      ],
+    })
+
+  const [client, setClient] = useState(makeClient())
+
+  return (
+    <ClientContext.Provider
+      value={{
+        resetClient: () => setClient(makeClient()),
+      }}
+    >
+      <Provider value={client}>{children}</Provider>
+    </ClientContext.Provider>
+  )
 }
