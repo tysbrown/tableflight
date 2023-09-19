6e7adefb165c86ac157c80719051ba706c04dcee
Tyler Brown <tylershaynebrown@gmail.com>
2023-09-19 01:52:49 -0400

    refactor(server): clean up server.ts to be more functional

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from "@apollo/server/express4"
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
-import express from "express"
+import express, { Request, Response } from "express"
 import http from "http"
 import cors from "cors"
 import bodyParser from "body-parser"
@@ -28,87 +28,90 @@ type Payload =
       userId: number
     }
 
-const app = express()
-const httpServer = http.createServer(app)
+const createServer = (httpServer: http.Server) => {
+  return new ApolloServer<ApolloServerContext>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer: httpServer })],
+  })
+}
 
-const server = new ApolloServer<ApolloServerContext>({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  // introspection: true,
-})
+const handleGraphQLContext = async ({
+  req,
+  res,
+}: {
+  req: Request
+  res: Response
+}) => {
+  const { authorization } = req.headers
 
-await server.start()
-
-app.use(cookieParser())
-
-app.use(
-  cors<cors.CorsRequest>({
-    origin: [
-      "http://localhost:5173",
-      "https://localhost:5173",
-      "https://sandbox.embed.apollographql.com",
-    ],
-    credentials: true,
-  }),
-)
-
-app.use(
-  "/graphql",
-  bodyParser.json(),
-  expressMiddleware(server, {
-    context: async ({ req, res }) => {
-      const { authorization } = req.headers
-
-      if (authorization) {
-        const token = authorization?.split(" ")[1]
-
-        const payload: Payload = verify(token, process.env.ACCESS_TOKEN_SECRET)
-        const { userId } = payload || {}
-        const user = await getUser(context, userId)
-        return {
-          ...context,
-          req,
-          res,
-          user,
-        }
-      }
-      return {
-        ...context,
-        req,
-        res,
-      }
-    },
-  }),
-)
-
-app.post("/refresh_token", async (req, res) => {
-  const token = req.cookies.jid
-
-  if (!token) {
-    return res.send({ ok: false, accessToken: "" })
+  if (authorization) {
+    const token = authorization.split(" ")[1]
+    const payload: Payload = verify(token, process.env.ACCESS_TOKEN_SECRET)
+    const user = await getUser(context, payload.userId)
+    return {
+      ...context,
+      req,
+      res,
+      user,
+    }
   }
+
+  return {
+    ...context,
+    req,
+    res,
+  }
+}
+
+const handleRefreshToken = async (req: Request, res: Response) => {
+  const token = req.cookies.jid
+  if (!token) return res.send({ ok: false, accessToken: "" })
 
   const payload: Payload = verify(token, process.env.REFRESH_TOKEN_SECRET)
-  const { userId } = payload || {}
-  const user = await getUser(context, userId)
-
-  if (!user) {
+  const user = await getUser(context, payload.userId)
+  if (!user || user.tokenVersion !== payload.tokenVersion)
     return res.send({ ok: false, accessToken: "" })
-  }
 
-  if (user.tokenVersion !== payload.tokenVersion) {
-    return res.send({ ok: false, accessToken: "" })
-  }
+  setRefreshTokenCookie(res, createRefreshToken(user))
+  return res.send({ ok: true, accessToken: createAccessToken(user), user })
+}
 
-  const refreshToken = createRefreshToken(user)
-  setRefreshTokenCookie(res, refreshToken)
-  const newToken = createAccessToken(user)
+const initServer = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
+  const server = createServer(httpServer)
 
-  return res.send({ ok: true, accessToken: newToken, user })
-})
+  await server.start()
 
-await new Promise<void>((resolve) => httpServer.listen({ port: 1337 }, resolve))
+  app.use(cookieParser())
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://localhost:5173",
+        "https://sandbox.embed.apollographql.com",
+      ],
+      credentials: true,
+    }),
+  )
 
-console.log("✨ UI ready at http://localhost:5173/")
-console.log("✨ GraphQL Playground ready at http://localhost:1337/graphql")
+  app.use(
+    "/graphql",
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: handleGraphQLContext,
+    }),
+  )
+
+  app.post("/refresh_token", handleRefreshToken)
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 1337 }, resolve),
+  )
+
+  console.log("✨ UI ready at http://localhost:5173/")
+  console.log("✨ GraphQL Playground ready at http://localhost:1337/graphql")
+}
+
+initServer()
