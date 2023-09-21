c3730b1de19923face39f518a2b8ae7dd0aaa52f
Tyler Brown <tylershaynebrown@gmail.com>
2023-09-21 01:14:18 -0400

    feat(components): custom slider

--- ui/src/components/organisms/HomeView.tsx
+++ ui/src/components/organisms/HomeView.tsx
@@ -1,9 +1,11 @@
-import React from "react"
 import type { Game } from "@/types"
+import { useState } from "react"
 import { gql, useQuery } from "urql"
-import Grid from "../atoms/Grid"
-import LoadingView from "../molecules/LoadingView"
 import tw from "twin.macro"
+import Grid from "../atoms/Grid"
+import SliderInput from "../atoms/SliderInput"
+import LoadingView from "../molecules/LoadingView"
+import Menu from "../molecules/Menu"
 
 const gamesQuery = gql`
   query Games {
@@ -19,15 +21,26 @@ const HomeView = () => {
   const [{ data, fetching, error }] = useQuery({
     query: gamesQuery,
   })
+  const [cellSize, setCellSize] = useState(50)
 
   if (fetching) return <LoadingView />
   if (error) return <p>Oh no... {error.message}</p>
 
   return (
-    <main css={[tw`relative w-full h-screen`]}>
-      <Grid cellSize={40} />
-
-      <section>
+    <main css={[tw`relative w-[2000px] h-[3000px]`]}>
+      <Grid cellSize={cellSize} lineWidth={0.5} />
+      <Menu>
+        <SliderInput
+          name="cellSize"
+          label="Cell Size"
+          value={cellSize}
+          setValue={setCellSize}
+          min={10}
+          max={100}
+          onChange={(e) => setCellSize(parseInt(e.target.value))}
+        />
+      </Menu>
+      <section css={[tw`absolute bottom-0 left-0`]}>
         <h1>Games:</h1>
         <ul>
           {data?.games?.map((game: Game) => <li key={game.id}>{game.name}</li>)}
