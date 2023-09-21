4235d8bd0a64bbf8cba83606d8feb749d02e678d
Tyler Brown <tylershaynebrown@gmail.com>
2023-09-21 03:30:22 -0400

    docs(utils): update tsdoc

--- ui/src/utils.ts
+++ ui/src/utils.ts
@@ -1,7 +1,10 @@
 /**
- * @function isFirefox
- * @memberof Utils
- * @description
+ * @module Utils
+ */
+
+/**
  * Check's the user agent to determine if the browser is Firefox.
+ * @memberof Utils
+ * @returns {boolean} True if the browser is Firefox, false otherwise.
  */
 export const isFirefox = !!navigator.userAgent.match(/firefox|fxios/i)
