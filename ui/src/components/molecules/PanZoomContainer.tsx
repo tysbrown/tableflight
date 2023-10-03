0ad56a5737a1c0ded8f0734d758bc089a5252ae8
Tyler <tylershaynebrown@gmail.com>
2023-10-02 21:35:24 -0400

    refactor(homeview): extract panning and zooming logic

---
+++ ui/src/components/molecules/PanZoomContainer.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useRef, useState } from "react"
+import tw from "twin.macro"
+
+type PanZoomContainerProps = {
+  image: HTMLImageElement | null
+  backgroundImage: string | null
+  isTokenDragging: boolean
+  setIsTokenDragging: React.Dispatch<React.SetStateAction<boolean>>
+  children: React.ReactNode
+}
+
+/**
+ * Container with custom panning and zooming functionality.
+ *
+ */
+const PanZoomContainer = ({
+  image,
+  backgroundImage,
+  isTokenDragging,
+  setIsTokenDragging,
+  children,
+}: PanZoomContainerProps) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [zoomLevel, setZoomLevel] = useState<number>(1)
+
+  const dragging = useRef(false)
+  const lastPosition = useRef({ x: 0, y: 0 })
+  const gridSectionRef = useRef<HTMLDivElement>(null)
+  const gridContainerRef = useRef<HTMLDivElement>(null)
+
+  const { current: gridSection } = gridSectionRef
+  const { current: gridContainer } = gridContainerRef
+
+  const gridWidth = gridContainer?.offsetWidth || 0
+  const gridHeight = gridContainer?.offsetHeight || 0
+
+  const originalWidth = image?.naturalWidth || gridWidth || 0
+  const originalHeight = image?.naturalHeight || gridHeight || 0
+
+  const handleZoom = (
+    e: React.WheelEvent,
+    gridContainer: HTMLDivElement,
+    image: HTMLImageElement,
+  ) => {
+    const zoomDelta = -e.deltaY * 0.001
+    const newZoom = Math.max(0.1, Math.min(5, zoomLevel + zoomDelta))
+
+    const effectiveWidth = originalWidth * newZoom
+    const effectiveHeight = originalHeight * newZoom
+
+    gridContainer.style.width = `${effectiveWidth}px`
+    gridContainer.style.height = `${effectiveHeight}px`
+    image.style.width = `${effectiveWidth}px`
+    image.style.height = `${effectiveHeight}px`
+
+    setZoomLevel(newZoom)
+  }
+
+  const handlePanning = (e: React.WheelEvent) => {
+    const dx = e.deltaX
+    const dy = e.deltaY
+
+    updatePosition(dx, dy)
+  }
+
+  const updatePosition = (
+    dx: number,
+    dy: number,
+    isInverted: boolean = false,
+  ) => {
+    const { current: gridSection } = gridSectionRef
+
+    if (!gridSection) return
+
+    const viewportWidth = gridSection.offsetWidth
+    const viewportHeight = gridSection.offsetHeight
+
+    const effectiveWidth = originalWidth * zoomLevel
+    const effectiveHeight = originalHeight * zoomLevel
+
+    const xPos = isInverted ? position.x + dx : position.x - dx
+    const yPos = isInverted ? position.y + dy : position.y - dy
+
+    const newPos = {
+      x: Math.min(200, Math.max(viewportWidth - effectiveWidth - 200, xPos)),
+      y: Math.min(200, Math.max(viewportHeight - effectiveHeight - 200, yPos)),
+    }
+
+    setPosition(newPos)
+  }
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (e.button !== 0) return
+
+    const isOnToken = (e.target as HTMLElement).dataset.istoken === "true"
+
+    if (isOnToken) {
+      setIsTokenDragging(true)
+      return
+    }
+
+    dragging.current = true
+    lastPosition.current = { x: e.clientX, y: e.clientY }
+  }
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (isTokenDragging) return
+    if (!dragging.current) return
+
+    const dx = e.clientX - lastPosition.current.x
+    const dy = e.clientY - lastPosition.current.y
+
+    updatePosition(dx, dy, true)
+    lastPosition.current = { x: e.clientX, y: e.clientY }
+  }
+
+  const handleMouseUp = () => {
+    dragging.current = false
+  }
+
+  const handleWheel = (e: React.WheelEvent) => {
+    if (!gridSection || !gridContainer) return
+
+    const isZooming = image && (e.ctrlKey || e.metaKey)
+
+    if (isZooming) handleZoom(e, gridContainer, image)
+    else handlePanning(e)
+  }
+
+  useEffect(() => {
+    const preventDefaultZoom = (e: WheelEvent) => {
+      if (e.ctrlKey) e.preventDefault()
+    }
+
+    window.addEventListener("wheel", preventDefaultZoom, { passive: false })
+
+    return () => {
+      window.removeEventListener("wheel", preventDefaultZoom)
+    }
+  }, [])
+
+  return (
+    <section
+      ref={gridSectionRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onWheel={handleWheel}
+      css={[tw`relative w-full h-screen overflow-hidden`]}
+    >
+      <div
+        ref={gridContainerRef}
+        css={[
+          tw`w-fit`,
+          `transform: translate(${position.x}px, ${position.y}px)`,
+          !backgroundImage && tw`w-screen h-screen`,
+        ]}
+      >
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export default PanZoomContainer
--- ui/src/components/organisms/HomeView.tsx
+++ ui/src/components/organisms/HomeView.tsx
@@ -1,5 +1,5 @@
 import type { Game, GridType, Token } from "@/types"
-import React, { useEffect, useRef, useState } from "react"
+import React, { useRef, useState } from "react"
 import { gql, useQuery } from "urql"
 import tw from "twin.macro"
 import Grid from "../molecules/Grid"
@@ -7,6 +7,7 @@ import SliderInput from "../atoms/SliderInput"
 import LoadingView from "../molecules/LoadingView"
 import Menu from "../molecules/Menu"
 import NewTokenPanel from "../molecules/NewTokenPanel"
+import PanZoomContainer from "../molecules/PanZoomContainer"
 
 const gamesQuery = gql`
   query Games {
@@ -27,16 +28,10 @@ const HomeView = () => {
     query: gamesQuery,
   })
 
+  const imageRef = useRef<HTMLImageElement | null>(null)
+
   const [cellSize, setCellSize] = useState(50)
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [zoomLevel, setZoomLevel] = useState<number>(1)
-  const dragging = useRef(false)
-  const lastPosition = useRef({ x: 0, y: 0 })
-  const gridContainerRef = useRef<HTMLDivElement>(null)
-  const imageRef = useRef<HTMLImageElement>(null)
-
-  const gridSectionRef = useRef<HTMLDivElement>(null)
 
   const rows = Math.ceil(dimensions.height / cellSize)
   const cols = Math.ceil(dimensions.width / cellSize)
@@ -45,15 +40,6 @@ const HomeView = () => {
   const [grid, setGrid] = useState<GridType>(initialGrid)
   const [backgroundImage, setBackgroundImage] = useState<string | null>(null)
   const [isTokenDragging, setIsTokenDragging] = useState<boolean>(false)
-
-  const { current: gridContainer } = gridContainerRef
-  const { current: image } = imageRef
-
-  const gridWidth = gridContainer?.offsetWidth || 0
-  const gridHeight = gridContainer?.offsetHeight || 0
-
-  const originalWidth = image?.naturalWidth || gridWidth || 0
-  const originalHeight = image?.naturalHeight || gridHeight || 0
 
   const addTokenToGrid = (x: number, y: number, token: Token) => {
     setGrid((prev) => {
@@ -72,32 +58,6 @@ const HomeView = () => {
     })
   }
 
-  const updatePosition = (
-    dx: number,
-    dy: number,
-    isInverted: boolean = false,
-  ) => {
-    const { current: gridSection } = gridSectionRef
-
-    if (!gridSection) return
-
-    const viewportWidth = gridSection.offsetWidth
-    const viewportHeight = gridSection.offsetHeight
-
-    const effectiveWidth = originalWidth * zoomLevel
-    const effectiveHeight = originalHeight * zoomLevel
-
-    const xPos = isInverted ? position.x + dx : position.x - dx
-    const yPos = isInverted ? position.y + dy : position.y - dy
-
-    const newPos = {
-      x: Math.min(200, Math.max(viewportWidth - effectiveWidth - 200, xPos)),
-      y: Math.min(200, Math.max(viewportHeight - effectiveHeight - 200, yPos)),
-    }
-
-    setPosition(newPos)
-  }
-
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null
 
@@ -112,122 +72,38 @@ const HomeView = () => {
     }
   }
 
-  const handleMouseDown = (e: React.MouseEvent) => {
-    if (e.button !== 0) return
-
-    const isOnToken = (e.target as HTMLElement).dataset.istoken === "true"
-
-    if (isOnToken) {
-      setIsTokenDragging(true)
-      return
-    }
-
-    dragging.current = true
-    lastPosition.current = { x: e.clientX, y: e.clientY }
-  }
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    if (isTokenDragging) return
-    if (!dragging.current) return
-
-    const dx = e.clientX - lastPosition.current.x
-    const dy = e.clientY - lastPosition.current.y
-
-    updatePosition(dx, dy, true)
-    lastPosition.current = { x: e.clientX, y: e.clientY }
-  }
-
-  const handleMouseUp = () => {
-    dragging.current = false
-  }
-
-  const handleWheel = (e: React.WheelEvent) => {
-    const { current: gridSection } = gridSectionRef
-    const { current: gridContainer } = gridContainerRef
-
-    // Only proceed if we have valid refs
-    if (!gridSection || !gridContainer) return
-
-    if (image && (e.ctrlKey || e.metaKey)) {
-      // Capture pinch-to-zoom
-      const zoomDelta = -e.deltaY * 0.001
-      const newZoom = Math.max(0.1, Math.min(5, zoomLevel + zoomDelta))
-
-      const effectiveWidth = originalWidth * newZoom
-      const effectiveHeight = originalHeight * newZoom
-
-      // Update DOM styles
-      gridContainer.style.width = `${effectiveWidth}px`
-      gridContainer.style.height = `${effectiveHeight}px`
-      image.style.width = `${effectiveWidth}px`
-      image.style.height = `${effectiveHeight}px`
-
-      setZoomLevel(newZoom)
-    } else {
-      // Handle panning
-      const dx = e.deltaX
-      const dy = e.deltaY
-
-      updatePosition(dx, dy)
-    }
-  }
-
-  useEffect(() => {
-    const preventDefaultZoom = (e: WheelEvent) => {
-      if (e.ctrlKey) e.preventDefault()
-    }
-
-    window.addEventListener("wheel", preventDefaultZoom, { passive: false })
-
-    return () => {
-      window.removeEventListener("wheel", preventDefaultZoom)
-    }
-  }, [])
-
   if (fetching) return <LoadingView />
   if (error) return <p>Oh no... {error.message}</p>
 
   return (
     <main css={[tw`flex overflow-hidden`]}>
-      <section
-        ref={gridSectionRef}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-        onWheel={handleWheel}
-        css={[tw`relative w-full h-screen overflow-hidden`]}
+      <PanZoomContainer
+        image={imageRef.current}
+        backgroundImage={backgroundImage}
+        isTokenDragging={isTokenDragging}
+        setIsTokenDragging={setIsTokenDragging}
       >
-        <div
-          ref={gridContainerRef}
-          css={[
-            tw`w-fit`,
-            `transform: translate(${position.x}px, ${position.y}px)`,
-            !backgroundImage && tw`w-screen h-screen`,
-          ]}
-        >
-          {backgroundImage && (
-            <img
-              src={backgroundImage}
-              alt="Background"
-              ref={imageRef}
-              css={[tw`max-w-none w-auto h-auto origin-center`]}
-            />
-          )}
-          <Grid
-            dimensions={dimensions}
-            grid={grid}
-            setDimensions={setDimensions}
-            addTokenToGrid={addTokenToGrid}
-            removeTokenFromGrid={removeTokenFromGrid}
-            rows={rows}
-            cols={cols}
-            cellSize={cellSize}
-            lineWidth={0.5}
-            setIsTokenDragging={setIsTokenDragging}
-            zoomLevel={zoomLevel}
+        {backgroundImage && (
+          <img
+            src={backgroundImage}
+            alt="Background"
+            ref={imageRef}
+            css={[tw`max-w-none w-auto h-auto origin-center`]}
           />
-        </div>
-      </section>
+        )}
+        <Grid
+          dimensions={dimensions}
+          grid={grid}
+          setDimensions={setDimensions}
+          addTokenToGrid={addTokenToGrid}
+          removeTokenFromGrid={removeTokenFromGrid}
+          rows={rows}
+          cols={cols}
+          cellSize={cellSize}
+          lineWidth={0.5}
+          setIsTokenDragging={setIsTokenDragging}
+        />
+      </PanZoomContainer>
 
       <Menu>
         <SliderInput
