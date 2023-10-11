7b9b620b89505937c7bf76d06fdf231da78556d2
Tyler <tylershaynebrown@gmail.com>
2023-10-10 23:23:15 -0400

    feat(button): add ref prop

--- ui/src/components/atoms/Button.tsx
+++ ui/src/components/atoms/Button.tsx
@@ -1,3 +1,4 @@
+import { forwardRef, LegacyRef } from "react"
 import tw from "twin.macro"
 
 type ButtonProps = {
@@ -5,36 +6,43 @@ type ButtonProps = {
   type: "submit" | "reset" | "button"
   disabled?: boolean
   onClick?: () => void
+  ref?: LegacyRef<HTMLButtonElement> | undefined
   children: React.ReactNode
 }
 
-const Button = ({
-  style,
-  type,
-  disabled,
-  onClick,
-  children,
-  ...remainingProps
-}: ButtonProps) => {
-  const styles = {
-    primary: tw`bg-primary border border-primary text-onPrimary primaryHoverOverlay primaryFocusOverlay`,
-    secondary: tw`bg-secondary text-onSecondary`,
-    outline: tw`bg-none border border-outline text-primary outlineHoverOverlay outlineFocusOverlay`,
-    tertiary: tw`bg-tertiary border border-tertiary text-onTertiary`,
-    link: tw`bg-none text-sm p-0 rounded-none text-primary underline`,
-  }
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      style,
+      type,
+      disabled,
+      onClick,
+      children,
+      ...remainingProps
+    }: ButtonProps,
+    ref,
+  ) => {
+    const styles = {
+      primary: tw`bg-primary border border-primary text-onPrimary primaryHoverOverlay primaryFocusOverlay`,
+      secondary: tw`bg-secondary text-onSecondary`,
+      outline: tw`bg-none border border-outline text-primary outlineHoverOverlay outlineFocusOverlay`,
+      tertiary: tw`bg-tertiary border border-tertiary text-onTertiary`,
+      link: tw`bg-none text-sm p-0 rounded-none text-primary underline`,
+    }
 
-  return (
-    <button
-      onClick={onClick}
-      css={[tw`font-medium w-fit px-6 py-2 rounded-full`, styles[style]]}
-      type={type}
-      disabled={disabled}
-      {...remainingProps}
-    >
-      {children}
-    </button>
-  )
-}
+    return (
+      <button
+        onClick={onClick}
+        css={[tw`font-medium w-fit px-6 py-2 rounded-full`, styles[style]]}
+        type={type}
+        disabled={disabled}
+        {...remainingProps}
+        ref={ref}
+      >
+        {children}
+      </button>
+    )
+  },
+)
 
 export default Button
