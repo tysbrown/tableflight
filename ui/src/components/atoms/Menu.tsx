57c722113a20fc51793edf2df0d4bb6a50f2c78c
Tyler <tylershaynebrown@gmail.com>
2023-10-11 23:01:39 -0400

    feat(menu): clean up component
    * Adds transitions.
    
    * Destructures variables for readability.

--- ui/src/components/atoms/Menu.tsx
+++ ui/src/components/atoms/Menu.tsx
@@ -15,51 +15,56 @@ type MenuProps = {
  * and the viewport.
  *
  * @remarks
- * 320 is the width of the control panel.
+ * - 320 is the width of the control panel.
+ * - You need a hard value for height in order to animate it, so we use the scrollHeight from the ref.
  */
-export const Menu = ({ isOpen, anchorElement, children }: MenuProps) => {
+export const Menu = ({
+  isOpen,
+  anchorElement,
+  children,
+  ...props
+}: MenuProps) => {
   const menuRef = useRef<HTMLUListElement | null>(null)
+  const { offsetWidth: menuOffsetWidth, scrollHeight: menuHeight } =
+    menuRef?.current || {}
 
-  if (!isOpen) return null
   if (!anchorElement.current) return null
 
-  const { top, left, right, height } =
-    anchorElement.current.getBoundingClientRect()
+  const { left, right, bottom } = anchorElement.current.getBoundingClientRect()
 
   const viewportWidth = window.innerWidth - 320
-
-  const menuWidth = menuRef?.current?.offsetWidth || 250
-
-  const tooCloseToRightEdge = right > viewportWidth - menuWidth - 20
-
-  const leftValue = tooCloseToRightEdge ? right - menuWidth : left
+  const menuWidth = menuOffsetWidth || 200
+  const isCloseToRightEdge = right > viewportWidth - menuWidth - 20
+  const leftValue = isCloseToRightEdge ? right - menuWidth : left
 
   return (
     <ul
       ref={menuRef}
       css={[
-        tw`absolute z-50 flex flex-col bg-surfaceContainer rounded-md overflow-visible`,
+        tw`absolute flex flex-col bg-surfaceContainer rounded-md overflow-hidden z-50 h-0 opacity-0`,
         `
-          top: ${top + height}px;
+          top: ${bottom + 2}px;
           left: ${leftValue}px;
+          transition: opacity 200ms linear, height 150ms ease-in-out;
         `,
+        isOpen && tw`opacity-100`,
+        isOpen && `height: ${menuHeight}px`,
       ]}
+      {...props}
     >
       {children}
     </ul>
   )
 }
 
-export const MenuItem = ({
-  children,
-  ...remainingProps
-}: {
-  children: ReactNode
-}) => {
+export const MenuItem = ({ children, ...props }: { children: ReactNode }) => {
   return (
     <li
-      css={[tw`bg-surfaceContainer py-2 px-3 min-w-[250px] overflow-visible`]}
-      {...remainingProps}
+      css={[
+        tw`bg-surfaceContainer h-10 py-2 px-3 min-w-[200px] cursor-pointer`,
+        tw`hover:bg-surfaceContainerHighest`,
+      ]}
+      {...props}
     >
       {children}
     </li>
