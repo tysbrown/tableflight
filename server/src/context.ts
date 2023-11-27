dcc7fa836e8e59f05400c64082e9961b13dcd295
Tyler <tylershaynebrown@gmail.com>
2023-11-26 21:44:38 -0500

    chore(server): migrate to prisma accelerate

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,8 +1,15 @@
 import type { User } from "@/types"
 import type { Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
+import { withAccelerate } from "@prisma/extension-accelerate"
+import {
+  DefaultArgs,
+  PrismaClientOptions,
+} from "@prisma/client/runtime/library"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends(
+  withAccelerate(),
+) as unknown as PrismaClient<PrismaClientOptions, never, DefaultArgs>
 
 export interface Context {
   prisma: PrismaClient
