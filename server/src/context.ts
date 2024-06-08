6f5fda8c21fc9f64c8c2fad8f10af0283e0b34f6
Tyler <tylershaynebrown@gmail.com>
2024-06-08 00:52:51 -0400

    revert(context): accelerate type

--- server/src/context.ts
+++ server/src/context.ts
@@ -2,8 +2,14 @@ import type { User } from "@/types"
 import type { Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
 import { withAccelerate } from "@prisma/extension-accelerate"
+import {
+  DefaultArgs,
+  PrismaClientOptions,
+} from "@prisma/client/runtime/library"
 
-const prisma = new PrismaClient().$extends(withAccelerate()) as unknown
+const prisma = new PrismaClient().$extends(
+  withAccelerate(),
+) as unknown as PrismaClient<PrismaClientOptions, never, DefaultArgs>
 
 export interface Context {
   prisma: PrismaClient
