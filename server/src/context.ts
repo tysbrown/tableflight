95f945d7d6795711a7c2b53fa0d15de996ed3a2e
Tyler <tylershaynebrown@gmail.com>
2024-06-08 00:48:05 -0400

    chore(context): remove accelerate types

--- server/src/context.ts
+++ server/src/context.ts
@@ -2,14 +2,8 @@ import type { User } from "@/types"
 import type { Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
 import { withAccelerate } from "@prisma/extension-accelerate"
-import {
-  DefaultArgs,
-  PrismaClientOptions,
-} from "@prisma/client/runtime/library"
 
-const prisma = new PrismaClient().$extends(
-  withAccelerate(),
-) as unknown as PrismaClient<PrismaClientOptions, never, DefaultArgs>
+const prisma = new PrismaClient().$extends(withAccelerate()) as unknown
 
 export interface Context {
   prisma: PrismaClient
