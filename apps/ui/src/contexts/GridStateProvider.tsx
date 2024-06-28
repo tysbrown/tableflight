540fdc88cc880a5b697a2b908d5ba5aa69046a01
Tyler <tylershaynebrown@gmail.com>
2024-06-28 19:14:29 -0400

    refactor(contexts): clean up grid state provider

--- apps/ui/src/contexts/GridStateProvider.tsx
+++ apps/ui/src/contexts/GridStateProvider.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useReducer } from "react"
-import { GridType, TokenType, type Canvas } from "~common"
+import { createContext, useEffect, useReducer } from 'react'
+import { GridType, TokenType, type Canvas } from '~common'
 
 export type GridState = {
   grid: GridType
@@ -11,7 +11,7 @@ export type GridState = {
   backgroundImage: string | null
   lineWidth: number
   zoomLevel: number
-  mode: "draw" | "pan"
+  mode: 'draw' | 'pan'
   canvas: Canvas
 }
 
@@ -27,117 +27,193 @@ type ActionType =
   | SetCanvasAction
 
 export type AddTokenAction = {
-  type: "ADD_TOKEN"
+  type: 'ADD_TOKEN'
   x: number
   y: number
   token: TokenType
 }
 
 export type RemoveTokenAction = {
-  type: "REMOVE_TOKEN"
+  type: 'REMOVE_TOKEN'
   x: number
   y: number
 }
-
 export type SetBackgroundAction = {
-  type: "SET_BACKGROUND"
+  type: 'SET_BACKGROUND'
   backgroundImage: string
 }
 
 export type SetDimensionsAction = {
-  type: "SET_DIMENSIONS"
+  type: 'SET_DIMENSIONS'
   dimensions: { width: number; height: number }
 }
 
 export type SetCellSizeAction = {
-  type: "SET_CELL_SIZE"
+  type: 'SET_CELL_SIZE'
   cellSize: number
 }
 
 export type SetZoomLevelAction = {
-  type: "SET_ZOOM_LEVEL"
+  type: 'SET_ZOOM_LEVEL'
   zoomLevel: number
 }
 
 export type SetGameSessionIdAction = {
-  type: "SET_GAME_SESSION_ID"
+  type: 'SET_GAME_SESSION_ID'
   gameSessionId: string
 }
 
 export type SetModeAction = {
-  type: "SET_MODE"
-  mode: "draw" | "pan"
+  type: 'SET_MODE'
+  mode: 'draw' | 'pan'
 }
 
 export type SetCanvasAction = {
-  type: "SET_CANVAS"
+  type: 'SET_CANVAS'
   canvas: Canvas
+}
+
+const addToken = (state: GridState, action: AddTokenAction): GridState => {
+  const { x, y, token } = action
+  const { grid } = state
+
+  const newGrid = grid.map((row, rowIndex) =>
+    row.map((cell, colIndex) => {
+      if (rowIndex === y && colIndex === x) {
+        return token
+      }
+
+      return cell
+    }),
+  )
+
+  return {
+    ...state,
+    grid: newGrid,
+  }
+}
+
+const removeToken = (
+  state: GridState,
+  action: RemoveTokenAction,
+): GridState => {
+  const { x, y } = action
+  const { grid } = state
+
+  const newGrid = grid.map((row, rowIndex) =>
+    row.map((cell, colIndex) => {
+      if (rowIndex === y && colIndex === x) {
+        return null
+      }
+
+      return cell
+    }),
+  )
+
+  return {
+    ...state,
+    grid: newGrid,
+  }
+}
+
+const setBackground = (
+  state: GridState,
+  action: SetBackgroundAction,
+): GridState => {
+  return {
+    ...state,
+    backgroundImage: action.backgroundImage,
+    canvas: {
+      lines: [], // TODO: Remove this when canvas is persisted to db
+    },
+  }
+}
+
+const setDimensions = (
+  state: GridState,
+  action: SetDimensionsAction,
+): GridState => {
+  return {
+    ...state,
+    dimensions: action.dimensions,
+    rows: Math.ceil(action.dimensions.height / state.cellSize),
+    cols: Math.ceil(action.dimensions.width / state.cellSize),
+    grid: Array.from({
+      length: Math.ceil(action.dimensions.height / state.cellSize),
+    }).map(() =>
+      Array.from({
+        length: Math.ceil(action.dimensions.width / state.cellSize),
+      }).fill(null),
+    ) as GridType,
+  }
+}
+
+const setCellSize = (
+  state: GridState,
+  action: SetCellSizeAction,
+): GridState => {
+  return {
+    ...state,
+    cellSize: action.cellSize,
+    rows: Math.ceil(state.dimensions.height / action.cellSize),
+    cols: Math.ceil(state.dimensions.width / action.cellSize),
+  }
+}
+
+const setZoomLevel = (
+  state: GridState,
+  action: SetZoomLevelAction,
+): GridState => {
+  return {
+    ...state,
+    zoomLevel: action.zoomLevel,
+  }
+}
+
+const setGameSessionId = (
+  state: GridState,
+  action: SetGameSessionIdAction,
+): GridState => {
+  return {
+    ...state,
+    gameSessionId: action.gameSessionId,
+  }
+}
+
+const setMode = (state: GridState, action: SetModeAction): GridState => {
+  return {
+    ...state,
+    mode: action.mode,
+  }
+}
+
+const setCanvas = (state: GridState, action: SetCanvasAction): GridState => {
+  return {
+    ...state,
+    canvas: action.canvas,
+  }
 }
 
 const gridReducer = (state: GridState, action: ActionType): GridState => {
   switch (action.type) {
-    case "ADD_TOKEN": {
-      const newGrid = [...state.grid]
-      if (!newGrid[action.y])
-        newGrid[action.y] = Array(state.cols).fill(null) as GridType[number]
-      newGrid[action.y]![action.x] = action.token
-
-      return {
-        ...state,
-        grid: newGrid,
-      }
-    }
-    case "REMOVE_TOKEN": {
-      const newGrid = [...state.grid]
-      newGrid[action.y]![action.x] = null
-
-      return {
-        ...state,
-        grid: newGrid,
-      }
-    }
-    case "SET_BACKGROUND":
-      return {
-        ...state,
-        backgroundImage: action.backgroundImage,
-        canvas: {
-          lines: [], // TODO: Remove this when canvas is persisted to db
-        },
-      }
-    case "SET_DIMENSIONS":
-      return {
-        ...state,
-        dimensions: action.dimensions,
-        rows: Math.ceil(action.dimensions.height / state.cellSize),
-        cols: Math.ceil(action.dimensions.width / state.cellSize),
-      }
-    case "SET_CELL_SIZE":
-      return {
-        ...state,
-        cellSize: action.cellSize,
-        rows: Math.ceil(state.dimensions.height / action.cellSize),
-        cols: Math.ceil(state.dimensions.width / action.cellSize),
-      }
-    case "SET_ZOOM_LEVEL":
-      return {
-        ...state,
-        zoomLevel: action.zoomLevel,
-      }
-    case "SET_GAME_SESSION_ID":
-      return {
-        ...state,
-        gameSessionId: action.gameSessionId,
-      }
-    case "SET_MODE":
-      return {
-        ...state,
-        mode: action.mode,
-      }
-    case "SET_CANVAS":
-      return {
-        ...state,
-        canvas: action.canvas,
-      }
+    case 'ADD_TOKEN':
+      return addToken(state, action)
+    case 'REMOVE_TOKEN':
+      return removeToken(state, action)
+    case 'SET_BACKGROUND':
+      return setBackground(state, action)
+    case 'SET_DIMENSIONS':
+      return setDimensions(state, action)
+    case 'SET_CELL_SIZE':
+      return setCellSize(state, action)
+    case 'SET_ZOOM_LEVEL':
+      return setZoomLevel(state, action)
+    case 'SET_GAME_SESSION_ID':
+      return setGameSessionId(state, action)
+    case 'SET_MODE':
+      return setMode(state, action)
+    case 'SET_CANVAS':
+      return setCanvas(state, action)
     default:
       return state
   }
@@ -158,7 +234,7 @@ const initialState: GridState = {
   lineWidth: 0.5,
   zoomLevel: 1,
   gameSessionId: null,
-  mode: "pan",
+  mode: 'pan',
   canvas: {
     lines: [],
   },
@@ -166,7 +242,7 @@ const initialState: GridState = {
 
 export const GridStateContext = createContext<GridContextType>({
   state: initialState,
-  dispatch: () => {},
+  dispatch: () => initialState,
 })
 
 export const GridStateProvider = ({
@@ -180,7 +256,7 @@ export const GridStateProvider = ({
    * @todo - Remove this useEffect when finished debugging
    */
   useEffect(() => {
-    console.log("GRID STATE changed to: ", state)
+    console.log('GRID STATE changed to: ', state)
   }, [state])
 
   return (
