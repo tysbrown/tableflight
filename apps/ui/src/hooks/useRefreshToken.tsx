6144f55e7e81f561b78d7addcceb159dd167980c
Tyler <tylershaynebrown@gmail.com>
2024-07-04 16:08:54 -0400

    refactor(hooks): use axios for fetching

--- apps/ui/src/hooks/useRefreshToken.tsx
+++ apps/ui/src/hooks/useRefreshToken.tsx
@@ -1,18 +1,20 @@
-import { User } from "~common"
-import { useGlobalState } from "./useGlobalState"
+import { User } from '~common'
+import { useGlobalState } from './useGlobalState'
+import axios from 'axios'
 
 export const useRefreshToken = () => {
   const { state, setState } = useGlobalState()
 
   return async () => {
     try {
-      const response = await fetch(import.meta.env.VITE_REFRESH_TOKEN_URL, {
-        method: "POST",
-        credentials: "include",
-      })
+      const { data } = await axios.post(
+        import.meta.env.VITE_REFRESH_TOKEN_URL,
+        {
+          credentials: 'include',
+        },
+      )
 
-      const { accessToken, user, ok } =
-        (await response.json()) as RefreshTokenResponse
+      const { accessToken, user, ok } = data as RefreshTokenResponse
 
       if (!ok) {
         setState({ ...state, isLoggedIn: false })
@@ -22,7 +24,7 @@ export const useRefreshToken = () => {
       setState({ ...state, accessToken, user, isLoggedIn: true })
       return true
     } catch (error) {
-      console.error("Error refreshing token: ", error)
+      console.error('Error refreshing token: ', error)
       return false
     }
   }
