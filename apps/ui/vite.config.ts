f271cfde5dd55376edb668996c8492245c950bc7
Tyler <tylershaynebrown@gmail.com>
2024-07-03 23:14:13 -0400

    fix: type imports

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import macrosPlugin from 'vite-plugin-babel-macros'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   root: __dirname,
@@ -47,6 +48,7 @@ export default defineConfig({
     }),
     macrosPlugin(),
     nxViteTsPaths(),
+    tsconfigPaths(),
   ],
 
   // Uncomment this if you are using workers.
