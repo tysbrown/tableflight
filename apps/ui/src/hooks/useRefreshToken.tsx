bbffd2d6a015af9084ace5325d6ff4d42247a242
Tyler <tylershaynebrown@gmail.com>
2024-07-05 01:30:24 -0400

    fix(ui): axios syntax bug

--- apps/ui/src/hooks/useRefreshToken.tsx
+++ apps/ui/src/hooks/useRefreshToken.tsx
@@ -1,18 +1,21 @@
-import { User } from "~common"
-import { useGlobalState } from "./useGlobalState"
+import { User } from '~common'
+import { useGlobalState } from './useGlobalState'
+import axios from 'axios'
 
 export const useRefreshToken = () => {
   const { state, setState } = useGlobalState()
 
   return async () => {
     try {
-      const response = await fetch(import.meta.env.VITE_REFRESH_TOKEN_URL, {
-        method: "POST",
-        credentials: "include",
-      })
+      const { data } = await axios.post(
+        import.meta.env.VITE_REFRESH_TOKEN_URL,
+        {},
+        {
+          withCredentials: true,
+        },
+      )
 
-      const { accessToken, user, ok } =
-        (await response.json()) as RefreshTokenResponse
+      const { accessToken, user, ok } = data as RefreshTokenResponse
 
       if (!ok) {
         setState({ ...state, isLoggedIn: false })
@@ -22,7 +25,7 @@ export const useRefreshToken = () => {
       setState({ ...state, accessToken, user, isLoggedIn: true })
       return true
     } catch (error) {
-      console.error("Error refreshing token: ", error)
+      console.error('Error refreshing token: ', error)
       return false
     }
   }
