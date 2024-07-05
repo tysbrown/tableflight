a7bf167378b3c9f5473760a2ad44ea40efc10e9c
Tyler <tylershaynebrown@gmail.com>
2024-07-05 00:13:40 -0400

    Revert "refactor(hooks): use axios for fetching"
    This reverts commit 6144f55e7e81f561b78d7addcceb159dd167980c.

--- apps/ui/src/hooks/useRefreshToken.tsx
+++ apps/ui/src/hooks/useRefreshToken.tsx
@@ -1,20 +1,18 @@
-import { User } from '~common'
-import { useGlobalState } from './useGlobalState'
-import axios from 'axios'
+import { User } from "~common"
+import { useGlobalState } from "./useGlobalState"
 
 export const useRefreshToken = () => {
   const { state, setState } = useGlobalState()
 
   return async () => {
     try {
-      const { data } = await axios.post(
-        import.meta.env.VITE_REFRESH_TOKEN_URL,
-        {
-          credentials: 'include',
-        },
-      )
+      const response = await fetch(import.meta.env.VITE_REFRESH_TOKEN_URL, {
+        method: "POST",
+        credentials: "include",
+      })
 
-      const { accessToken, user, ok } = data as RefreshTokenResponse
+      const { accessToken, user, ok } =
+        (await response.json()) as RefreshTokenResponse
 
       if (!ok) {
         setState({ ...state, isLoggedIn: false })
@@ -24,7 +22,7 @@ export const useRefreshToken = () => {
       setState({ ...state, accessToken, user, isLoggedIn: true })
       return true
     } catch (error) {
-      console.error('Error refreshing token: ', error)
+      console.error("Error refreshing token: ", error)
       return false
     }
   }
