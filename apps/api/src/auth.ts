78a74ff30eaf4f6549f23a374702af9b9a05b542
Tyler Brown <tylershaynebrown@gmail.com>
2024-07-07 03:20:27 -0400

    debug

--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -40,6 +40,7 @@ export const getUser = async ({ prisma }: Partial<Context>, userId: number) => {
 export const setRefreshTokenCookie = async (res: Response, token: string) => {
   try {
     res.cookie('jid', token, {
+      path: '/',
       httpOnly: true,
       secure: true,
       sameSite: 'none',
