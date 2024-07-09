3c33ddafadb9424b3e3b1a77bc80dbcb5c4eb81d
Tyler <tylershaynebrown@gmail.com>
2024-07-08 22:16:07 -0400

    debug

--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -43,7 +43,7 @@ export const setRefreshTokenCookie = async (res: Response, token: string) => {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
-      domain: isProd ? 'stage.tableflight.com' : 'localhost',
+      domain: isProd ? 'api.stage.tableflight.com' : 'localhost',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
   } catch (err) {
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -82,7 +82,13 @@ app.use(cookieParser())
 app.use(
   cors({
     credentials: true,
-    origin: 'https://stage.tableflight.com',
+    origin: [
+      'http://localhost:5173',
+      'https://sandbox.embed.apollographql.com',
+      'https://stage.tableflight.com',
+      'https://api.stage.tableflight.com',
+      'https://tableflight.com',
+    ],
   }),
 )
 app.use(bodyParser.json())
