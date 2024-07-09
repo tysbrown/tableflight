3d1b2ef57f3149b5444e4d15955436490e252b44
Tyler <tylershaynebrown@gmail.com>
2024-07-08 20:53:50 -0400

    revert(api): remove accellerate

--- apps/api/src/context.ts
+++ apps/api/src/context.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Context } from '~common'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends(
+  withAccelerate(),
+) as unknown as PrismaClient
 
 export const context: Partial<Context> = {
   prisma: prisma,
