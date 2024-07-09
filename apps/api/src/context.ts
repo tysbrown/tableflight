bfa7ba054ea0b35361cc4de04b6f71e5b5489a23
Tyler <tylershaynebrown@gmail.com>
2024-07-08 20:31:03 -0400

    chore(context): remove accellerate

--- apps/api/src/context.ts
+++ apps/api/src/context.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-// import { withAccelerate } from '@prisma/extension-accelerate'
 import { Context } from '~common'
-
-// const prisma = new PrismaClient().$extends(
-//   withAccelerate(),
-// ) as unknown as PrismaClient
 
 const prisma = new PrismaClient()
 
