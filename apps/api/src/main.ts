fdc66bfd6efa14d80761791f455f7c6d9954ee81
Tyler <tylershaynebrown@gmail.com>
2024-07-11 05:13:54 -0400

    debug

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -103,6 +103,8 @@ app.use('/graphql', yoga as RequestHandler)
 app.post('/refresh_token', handleRefreshToken)
 app.use('/api/graphql', yoga as RequestHandler)
 app.post('/api/refresh_token', handleRefreshToken)
+app.post('/graphql', yoga as RequestHandler)
+app.post('/api/graphql', yoga as RequestHandler)
 
 // Function to list all routes
 const listRoutes = (app: Express) => {
