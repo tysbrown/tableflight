6fe79bfcd62140baf2522ad5f44387bfe0cf5823
Tyler <tylershaynebrown@gmail.com>
2024-11-11 21:13:25 -0500

    feat(libs): generate profile lib

---
+++ libs/profile/src/lib/profile.spec.ts
@@ -0,0 +1,7 @@
+import { profile } from './profile'
+
+describe('profile', () => {
+  it('should work', () => {
+    expect(profile()).toEqual('profile')
+  })
+})
---
+++ libs/profile/src/lib/profile.ts
@@ -0,0 +1,3 @@
+export function profile(): string {
+  return 'profile'
+}
